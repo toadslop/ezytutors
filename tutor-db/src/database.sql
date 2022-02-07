dc180431bb6932ea71bd3b81a7cee5548972921d
Brian Heise <bnheise@gmail.com>
2022-02-08 08:22:43 +0900

    iter3 complete

--- tutor-db/src/database.sql
+++ tutor-db/src/database.sql
@@ -10,7 +10,7 @@ create table ezy_course_c4
 
 insert into ezy_course_c4
     (course_id, tutor_id, course_name, posted_time)
-values(1, 1, 'First course', '2020-12-17 05:40:00');
+values(DEFAULT, 1, 'First course', '2020-12-17 05:40:00');
 insert into ezy_course_c4
     (course_id, tutor_id, course_name, posted_time)
-values(2, 1, 'Second course', '2020-12-17 05:45:00');
+values(DEFAULT, 1, 'Second course', '2020-12-17 05:45:00');
--- tutor-db/src/iter3/handlers.rs
+++ tutor-db/src/iter3/handlers.rs
@@ -1,7 +1,9 @@
+use super::db_access::*;
 use super::state::AppState;
-use actix_web::{web, HttpResponse};
 use super::models::Course;
-use sqlx::postgres::PgPool;
+use std::convert::TryFrom;
+
+use actix_web::{web, HttpResponse};
 
 pub async fn health_check_handler(app_state: web::Data<AppState>) -> HttpResponse {
     let health_check_response = &app_state.health_check_response;
@@ -15,21 +17,29 @@ pub async fn post_new_course(
     new_course: web::Json<Course>,
     app_state: web::Data<AppState>,
 ) -> HttpResponse {
-    HttpResponse::Ok().json("success")
+    let course = post_new_course_db(&app_state.db, new_course.into()).await;
+
+    HttpResponse::Ok().json(course)
 }
 
 pub async fn get_courses_for_tutor(
     app_state: web::Data<AppState>,
     params: web::Path<usize>,
 ) -> HttpResponse {
-    HttpResponse::Ok().json("success")
+    let tutor_id = i32::try_from(params.0).unwrap();
+    let courses = get_courses_for_tutor_db(&app_state.db, tutor_id).await;
+    HttpResponse::Ok().json(courses)
 }
 
 pub async fn get_course_detail(
     app_state: web::Data<AppState>,
     params: web::Path<(usize, usize)>,
 ) -> HttpResponse {
-    HttpResponse::Ok().json("success")
+    let tuple = params.0;
+    let tutor_id: i32 = i32::try_from(tuple.0).unwrap();
+    let course_id: i32 = i32::try_from(tuple.1).unwrap();
+    let course = get_course_details_db(&app_state.db, tutor_id, course_id).await;
+    HttpResponse::Ok().json(course)
 }
 
 #[cfg(test)]
@@ -88,7 +98,7 @@ mod tests {
         let new_course_msg = Course {
             course_id: 1,
             tutor_id: 1,
-            course_name: "This si the next course".into(),
+            course_name: "This is the next course".into(),
             posted_time: Some(NaiveDate::from_ymd(2020, 8, 17).and_hms(14, 01, 11)),
         };
 
