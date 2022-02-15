745462886c4e0b4250080c9179db2338368ed9b9
brian heise <bnheise@gmail.com>
2022-02-15 21:51:50 +0900

    can now handle posting from a form

--- tutor-web-app-ssr/src/main.rs
+++ tutor-web-app-ssr/src/main.rs
@@ -1,6 +1,7 @@
 use actix_files as fs;
 use actix_web::{error, web, App, Error, HttpResponse, HttpServer, Result};
 use tera::Tera;
+use serde::{Deserialize, Serialize};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -11,15 +12,16 @@ async fn main() -> std::io::Result<()> {
 
         App::new()
             .data(tera)
-            .service(fs::Files::new("/static", "./static").show_files_listing())
-            .service(web::resource("/").route(web::get().to(index)))
+            .configure(app_config)
     })
     .bind("127.0.0.1:8080")?
     .run()
     .await
 }
 
-async fn index(tmpl: web::Data<tera::Tera>) -> Result<HttpResponse, Error> {
+async fn index(
+    tmpl: web::Data<tera::Tera>
+) -> Result<HttpResponse, Error> {
     let mut ctx = tera::Context::new();
     ctx.insert("name", "Bob");
     let s = tmpl
@@ -27,4 +29,43 @@ async fn index(tmpl: web::Data<tera::Tera>) -> Result<HttpResponse, Error> {
         .map_err(|_| error::ErrorInternalServerError("Template error"))?;
     
     Ok(HttpResponse::Ok().content_type("text/html").body(s))
+}
+
+async fn form(
+    tmpl: web::Data<tera::Tera>
+) -> Result<HttpResponse, Error> {
+        let s = tmpl
+        .render("form.html", &tera::Context::new())
+        .map_err(|_| error::ErrorInternalServerError("Template error"))?;
+    
+    Ok(HttpResponse::Ok().content_type("text/html").body(s))
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Tutor {
+    name: String,
+}
+
+async fn handle_post_tutor(
+    tmpl: web::Data<tera::Tera>,
+    params: web::Form<Tutor>,
+) -> Result<HttpResponse, Error> {
+    let mut ctx = tera::Context::new();
+    ctx.insert("name", &params.name);
+    ctx.insert("text", "Welcome!");
+    let s = tmpl
+        .render("user.html", &ctx)
+        .map_err(|_| error::ErrorInternalServerError("Template error"))?;
+    
+    Ok(HttpResponse::Ok().content_type("text/html").body(s))
+}
+
+fn app_config(config: &mut web::ServiceConfig) {
+    config.service(
+        web::scope("")
+            .service(fs::Files::new("/static", "./static").show_files_listing())
+            .service(web::resource("/").route(web::get().to(index)))
+            .service(web::resource("/form").route(web::get().to(form)))
+            .service(web::resource("/tutors").route(web::post().to(handle_post_tutor)))
+    );
 }
