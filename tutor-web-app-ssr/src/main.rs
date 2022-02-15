38ba3c53beb05dd2eef41c8e86b2e5e3c2f0aba0
brian heise <bnheise@gmail.com>
2022-02-15 21:14:36 +0900

    can serve static pages and tera templates

--- tutor-web-app-ssr/src/main.rs
+++ tutor-web-app-ssr/src/main.rs
@@ -1,14 +1,30 @@
 use actix_files as fs;
 use actix_web::{error, web, App, Error, HttpResponse, HttpServer, Result};
+use tera::Tera;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("Listening on 127.0.0.1:8080!");
     HttpServer::new(|| {
+        let tera = Tera::new(concat!(env!("CARGO_MANIFEST_DIR"), "/static/templates/**/*"))
+        .unwrap();
+
         App::new()
-        .service(fs::Files::new("/static", "./static").show_files_listing())
+            .data(tera)
+            .service(fs::Files::new("/static", "./static").show_files_listing())
+            .service(web::resource("/").route(web::get().to(index)))
     })
     .bind("127.0.0.1:8080")?
     .run()
     .await
+}
+
+async fn index(tmpl: web::Data<tera::Tera>) -> Result<HttpResponse, Error> {
+    let mut ctx = tera::Context::new();
+    ctx.insert("name", "Bob");
+    let s = tmpl
+        .render("index.html", &ctx)
+        .map_err(|_| error::ErrorInternalServerError("Template error"))?;
+    
+    Ok(HttpResponse::Ok().content_type("text/html").body(s))
 }
